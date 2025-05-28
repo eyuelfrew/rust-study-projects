4e067e1e3ad63b048910250674b9f92bb8d9fda7
Eyuel Frew <eyuelfrew430@gmail.com>
2025-05-28 17:15:12 +0300

    boom, first simple calculator app with only addition feature implimented

--- simple-calculator/src/main.rs
+++ simple-calculator/src/main.rs
@@ -1,4 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello World!");
+    println!("🦀 Welcome to the simpleest simple calculator! ");
+
+    // First number (write the first number and press enter in the cmd/terminal)
+    println!("Enter the first number: ");
+
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1).expect("Faild to read line(input)!");
+    let num1: f64 = input1.trim().parse().expect("Please enter a number");
+    // go for the second number 
+    
+    // Second number (same goes here like the first number)
+    println!("Enter second number: ");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Faild to read line(input)!");
+    let num2: f64 = input2.trim().parse().expect("Please enter a number!");
+    
+    // for know let do the addition, 
+    let sum = num1 + num2;
+
+    // and print the result in the terminal/cmd 
+    println!("The sum is : {}", sum);
+
+
+   
+    
+
 }
+
